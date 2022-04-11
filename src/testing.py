e391ebc9c16ea68c316ae6cad513ab64ec74cb0d
alapaix06 <102183125+alapaix06@users.noreply.github.com>
2022-04-11 14:44:05 -0400

    as

--- src/testing.py
+++ src/testing.py
@@ -11,12 +11,12 @@ class preRegister(unittest.TestCase):
         self.navDriver = webdriver.Chrome()
 
     def test_completePreregister(self):
-       
+
         navDriver = self.navDriver
 
         navDriver.get(
             'https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_custrec_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
-        
+
         insertEmail = navDriver.find_element_by_xpath(
             '/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div[1]/form/div/div/div/div[1]/input[1]')
         insertEmail.send_keys('adamslapaix.castillo@gmail.com')
@@ -32,7 +32,7 @@ class preRegister(unittest.TestCase):
             '/html/body/div[1]/header/div/div[1]/div[2]/div/form/div[3]/div[1]/input')
         insertArticle.send_keys('Apple Watch')
         insertArticle.send_keys(Keys.RETURN)
-        
+
         self.assertIn('Succesfull', navDriver.title)
 
         assert "Not found key in:" not in navDriver.page_source
@@ -43,6 +43,7 @@ class preRegister(unittest.TestCase):
 
     def tearDown(self):
         self.navDriver.close()
+
 
 if __name__ == '__main__':
     preRegister(unittest.main())
